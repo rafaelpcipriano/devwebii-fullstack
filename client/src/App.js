39046a7817ba31a6c81b1c4b695c27c6dccd27e8
Rafael Pereira Cipriano <rafaelcipriano@yahoo.com.br>
2025-07-03 23:30:28 -0300

    Publica projeto fullstack com frontend e backend

---
+++ client/src/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+function App() { return <h1>Hello Fullstack</h1>; }
+export default App;
---
+++ server/src/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('API online'));
+app.listen(3001);
